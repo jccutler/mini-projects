f19c5a78477719151512c525a31b8cd516fe8391
jccutler <joesfcutler@gmail.com>
2023-08-30 16:12:21 +0200

    Restructured visualiser using classes with the eventual goal of implementing interactive re-rendering

--- MathVis/mandelbrot.py
+++ MathVis/mandelbrot.py
@@ -2,9 +2,66 @@ import numpy as np
 import sys
 from numba import jit
 import matplotlib.pyplot as plt
+from matplotlib.widgets import RectangleSelector
+from screeninfo import get_monitors
+
+class MandelVis:
+    def __init__(self, x_init, y_init, depth, res):
+        self.x_range = x_init
+        self.y_range = y_init
+        self.width = self.x_range[1] - self.x_range[0]
+        self.height = self.y_range[1] - self.y_range[0]
+        self.depth = depth
+        self.res = res
+
+        pixels = self.render_mandelbrot() 
+        fig, ax = plt.subplots(figsize=self.initial_size(), dpi=100)
+        ax.imshow(pixels, cmap='inferno', interpolation='bilinear')
+        ax.axis('off')
+        plt.subplots_adjust(left=0, right=1, top=1, bottom=0, wspace=0, hspace=0)
+        plt.margins(0,0)
+        plt.show()
+                
+    def initial_size(self):
+        # Get largest possible size for figure while preserving aspect ratio 
+
+        monitor = get_monitors()[0]
+        dpi = 100 
+        width_in = monitor.width / dpi
+        height_in = monitor.height / dpi
+        
+        aspect_ratio = self.width / self.height
+
+        fig_height = height_in
+        fig_width = fig_height * aspect_ratio
+        if fig_width > width_in:
+            fig_height = fig_height * (width_in / fig_width)
+            fig_width = width_in
+
+        return (fig_width, fig_height)
+
+    def render_mandelbrot(self):
+        # Generate pixel array and get normalized index value for each pixel 
+
+        re_slices = int(self.width * self.res)
+        im_slices = int(self.height * self.res)
+
+        pixel_levels = np.empty((im_slices, re_slices))
+
+        for i in range(re_slices):
+            for j in range(im_slices):
+                x = self.x_range[0] + i/self.res
+                y = self.y_range[0] + j/self.res
+                index = mandelbrot_index(x, y, self.depth)
+                pixel_levels[j,i] = index / self.depth
+
+        return pixel_levels
+
 
 @jit(nopython=True)
 def mandelbrot_index(x_0, y_0, depth):
+    # Generates sequence with initial values x_0 y_0 until divergence OR depth is reached
+
     x = 0
     y = 0
     i = 0
@@ -14,24 +71,6 @@ def mandelbrot_index(x_0, y_0, depth):
         x = temp
         i += 1
     return i
-        
-def render_mandelbrot(x_range, y_range, depth, res):
-    width = x_range[1] - x_range[0]
-    height = y_range[1] - y_range[0]
-
-    re_slices = int(width * res)
-    im_slices = int(height * res)
-
-    pixel_levels = np.empty((im_slices, re_slices))
-
-    for i in range(re_slices):
-        for j in range(im_slices):
-            x = x_range[0] + i/res
-            y = y_range[0] + j/res
-            index = mandelbrot_index(x, y, depth)
-            pixel_levels[j,i] = index / depth
-
-    return pixel_levels
 
 def main():
     if len(sys.argv) < 5:
@@ -43,11 +82,7 @@ def main():
     depth = int(sys.argv[3])
     res_init = int(sys.argv[4])
 
-    pixels = render_mandelbrot(x_range_init, y_range_init, depth, res_init) 
-    plt.imshow(pixels, cmap='inferno', interpolation='bilinear')
-    plt.axis('off')
-    plt.tight_layout()
-    plt.show()
+    mandelbrot = MandelVis(x_range_init, y_range_init, depth, res_init)
 
 if __name__ == "__main__":
     main()
